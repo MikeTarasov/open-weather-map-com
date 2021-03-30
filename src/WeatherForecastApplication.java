04a9bcaae46fefffbbbcf7b157bd6c79b2e54938
MikeTarasov <59611048+miketarasov@users.noreply.github.com>
2021-03-30 20:00:43 +0300

    init

---
+++ src/WeatherForecastApplication.java
@@ -0,0 +1,10 @@
+public class WeatherForecastApplication {
+
+    public static void main(String[] args) {
+        new WeatherForecastApplication().run();
+    }
+
+    public void run() {
+        System.out.println("run");
+    }
+}
